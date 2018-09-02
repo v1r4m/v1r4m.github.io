640ac58921f382317f7ba2c464f7ee327c37f629
v1r4m <starsoulF@live.cau.ac.kr>
2018-09-03 01:04:03 +0900

    basic
    basic

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ $(function(){
 	// Trigger maximage
 	jQuery('#maximage').maximage();
 });
+
+$(function(d){
+	var img = d.getElementById("logo");
+	img.onmouseover = function ()
+	{
+		this.src = "logo1.png";
+	};
+		img.onmouseout = function ()
+	{
+			this.src = "logo2.png"; 
+	};
+}
